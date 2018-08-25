e64f15a8e099912abe81893ef474aa1e021d3184
b.blach <b.blachowski@wp.pl>
2018-08-25 14:13:22 +0200

    Adding base entity wich all model classes implements, closes issue #10

--- pet-clinic-data/src/main/java/org/rart/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/org/rart/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package org.rart.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends  BaseEntity{
     private LocalDate birthDate;
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/org/rart/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/org/rart/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package org.rart.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private  String name;
 

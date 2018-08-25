b84fb379ca44d93839cf9c54a2637a67e1848608
b.blach <b.blachowski@wp.pl>
2018-08-25 16:30:29 +0200

    Implement map based services, closes issue #6

---
+++ pet-clinic-data/src/main/java/org/rart/petclinic/services/map/VetService.java
@@ -0,0 +1,35 @@
+package org.rart.petclinic.services.map;
+
+import org.rart.petclinic.model.Vet;
+import org.rart.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetService extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+}

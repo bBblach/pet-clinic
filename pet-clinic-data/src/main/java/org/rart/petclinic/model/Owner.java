54095064ee718de33b445236fd8bef2e52719d49
b.blach <b.blachowski@wp.pl>
2018-08-25 11:29:19 +0200

    refactoring to multi module project, closing issue #1

--- pet-clinic-data/src/main/java/org/rart/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/rart/petclinic/model/Owner.java
@@ -1,4 +1,4 @@
 package org.rart.petclinic.model;
 
-public class Owner extends  Person {
+public class Owner extends Person {
 }
--- pet-clinic-data/src/main/java/org/rart/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/rart/petclinic/model/Vet.java
@@ -1,5 +1,5 @@
 package org.rart.petclinic.model;
 
 public class Vet extends  Person {
-    
+
 }

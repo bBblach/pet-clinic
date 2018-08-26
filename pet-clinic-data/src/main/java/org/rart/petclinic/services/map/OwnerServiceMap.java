b74e348099c55d872ae8a3bfaec3f699f078157b
b.blach <b.blachowski@wp.pl>
2018-08-26 11:31:13 +0200

    fixed AbstractMapService, was class now abstract

--- pet-clinic-data/src/main/java/org/rart/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/org/rart/petclinic/services/map/OwnerServiceMap.java
@@ -5,31 +5,27 @@ import org.rart.petclinic.services.CrudService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements CrudService<Owner,Long>  {
+public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements CrudService<Owner,Long> {
 
-
-    @Override
-    public Owner findById(Long id) {
-        return super.findById(id);
-    }
-
-    @Override
-    public Owner save(Owner object) {
-        return super.save(object.getId(),object);
-    }
 
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
     }
-
     @Override
-    public void deleteById(Long id) {
-        super.deleteById(id);
+    public Owner findById(Long id) {
+        return super.findById(id);
     }
-
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
     @Override
     public void delete(Owner object) {
         super.delete(object);
+    }
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
     }
 }

cbdd265a733361d5f32e2fc9fc8962a0cf562bc3
b.blach <b.blachowski@wp.pl>
2018-08-27 15:32:52 +0200

    Create PetType Map Service Impl close #30

---
+++ pet-clinic-data/src/main/java/org/rart/petclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package org.rart.petclinic.services;
+
+import org.rart.petclinic.models.PetType;
+
+public interface PetTypeService extends  CrudService<PetType,Long>{
+
+
+
+
+}
---
+++ pet-clinic-data/src/main/java/org/rart/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,38 @@
+package org.rart.petclinic.services.map;
+
+import org.rart.petclinic.models.PetType;
+import org.rart.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends  AbstractMapService<PetType,Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}

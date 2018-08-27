e7c93ea7d7f04be5b6889ee0b036039412f83567
b.blach <b.blachowski@wp.pl>
2018-08-27 15:15:02 +0200

    Create Vet Speciality Entity Asociate to Vet close #28

---
+++ pet-clinic-data/src/main/java/org/rart/petclinic/models/Specialty.java
@@ -0,0 +1,15 @@
+package org.rart.petclinic.models;
+
+public class Specialty extends  BaseEntity{
+
+    private  String description ;
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/org/rart/petclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/org/rart/petclinic/models/Vet.java
@@ -1,5 +1,17 @@
 package org.rart.petclinic.models;
 
+import java.util.Set;
+
 public class Vet extends  Person {
 
+    private Set<Specialty> specialties;
+
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }

b5e5972346dc7861ce766fdbdc63362531b207d7
b.blach <b.blachowski@wp.pl>
2018-08-27 09:43:09 +0200

    Udapte map impl to manage setting of id property. close  #23

--- pet-clinic-data/src/main/java/org/rart/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/org/rart/petclinic/services/map/AbstractMapService.java
@@ -1,13 +1,15 @@
 package org.rart.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.rart.petclinic.models.BaseEntity;
 
-public class AbstractMapService<T,ID> {
+import java.util.*;
 
-    protected Map<ID,T> map = new HashMap<>();
+public class AbstractMapService<T extends BaseEntity,ID extends  Long> {
+
+    final static Logger logger = Logger.getLogger(AbstractMapService.class);
+    protected Map<Long,T> map = new HashMap<>();
 
     Set<T> findAll(){
         return  new HashSet<>(map.values());
@@ -16,8 +18,16 @@ public class AbstractMapService<T,ID> {
         return map.get(id);
     }
 
-    T save(ID id , T object){
-        map.put(id, object);
+    T save(T object){
+        if (object !=null){
+            if (object.getId() == 0){
+                object.setId(getNextId());
+
+            }
+            map.put(object.getId(), object);
+        }else{
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
@@ -29,9 +39,19 @@ public class AbstractMapService<T,ID> {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 
-
+    private Long getNextId(){
+        Long  nextId = 0L;
+        try{
+            nextId = Collections.max(map.keySet()) + 1L;
+        }catch (NoSuchElementException e){
+            nextId=1L;
+            logger.log(Level.DEBUG, "map is empty, assiging nextId to 1 ");
+        }
+        return nextId;
+    }
     int size(){
-        return map.size();
+        int size =  map.size();
+        return size;
     }
 
 }

15d584db8c9bdefdca0f001fefa39dff0cd66995
b.blach <b.blachowski@wp.pl>
2018-08-27 15:17:54 +0200

    Add Contact Info Properties to owners close #29

--- pet-clinic-data/src/main/java/org/rart/petclinic/models/Owner.java
+++ pet-clinic-data/src/main/java/org/rart/petclinic/models/Owner.java
@@ -4,8 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String adress;
+    private String city;
+    private  String telphone;
     private Set<Pet> pets;
 
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
